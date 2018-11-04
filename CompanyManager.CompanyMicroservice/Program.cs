e988f6ec2cde40aebdfcd9a36a0d6f35d1231df6
Filirien <filirien@abv.bg>
2018-11-04 13:40:30 +0200

    CompanyMicroservice, DAL and Models initial commit

--- CompanyManager.CompanyMicroservice/Program.cs
+++ CompanyManager.CompanyMicroservice/Program.cs
@@ -2,11 +2,11 @@
 
 namespace CompanyManager.CompanyMicroservice
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
---
+++ CompanyManager.DAL/CompanyDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompanyManager.DAL
+{
+    public class CompanyDbContext
+    {
+
+    }
+}
